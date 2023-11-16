da1adc42b9ef3db328e7a078ea54e6f6af3ba334
Arbureanu-Teodor-vinci <teodor.arbureanu@student.vinci.be>
2023-11-16 18:56:13 +0100

    add: test with undefined name

--- spec/greetSpec.js
+++ spec/greetSpec.js
@@ -12,4 +12,9 @@ describe('A suite of test for greet', function () {
 
     expect(result).toBe('Hello, my friend')
   })
+  it('greeeting with undefined returns "Hello, my friend', function() {
+    const result = greet.greet()
+
+    expect(result).toBe('Hello, my friend')
+  })
 })
