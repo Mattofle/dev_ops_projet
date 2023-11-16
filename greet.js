65e6bd59a27df76abc0b7393f97808f557f0e23a
Erismann Matteo <matteo.erismann@student.vinci.be>
2023-11-16 18:34:39 +0100

    add: firt test simple greet

--- greet.js
+++ greet.js
@@ -1,6 +1,6 @@
 module.exports = {
 
-    greet() {
-        return "Hello, Bob"
+    greet(name) {
+        return "Hello, " + name;
     }
 }
--- spec/greetSpec.js
+++ spec/greetSpec.js
@@ -3,8 +3,8 @@ const greet = require('../greet');
 describe("A suite of test for greet", function() {
 
     it("greeting returns given hello, + Bob", function() {
-        let result = greet.greet();
+        let result = greet.greet("Bob");
 
         expect(result).toBe("Hello, Bob");
-    })
+    });
 })
