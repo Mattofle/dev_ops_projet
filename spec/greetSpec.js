6f2f23e4f87d8c008c068159ebe64d0cc71174bd
Arbureanu-Teodor-vinci <teodor.arbureanu@student.vinci.be>
2023-11-16 18:59:13 +0100

    add: test with empty string name BIS

--- spec/greetSpec.js
+++ spec/greetSpec.js
@@ -12,12 +12,12 @@ describe('A suite of test for greet', function () {
 
     expect(result).toBe('Hello, my friend')
   })
-  it('greeeting with undefined returns "Hello, my friend', function() {
+  it('greeeting with undefined returns "Hello, my friend', function () {
     const result = greet.greet()
 
     expect(result).toBe('Hello, my friend')
   })
-  it('greeeting with empty string returns "Hello, my friend', function() {
+  it('greeeting with empty string returns "Hello, my friend', function () {
     const result = greet.greet('')
 
     expect(result).toBe('Hello, my friend')
